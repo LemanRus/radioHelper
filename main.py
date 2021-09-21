251091e3e34f121c8cb8452c7cd805e609830e68
LemanRuss <wlad163163@gmail.com>
2021-09-21 16:47:49 +0400

    v0.0.1 Tests

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from kivy.factory import Factory
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.bottomnavigation import MDBottomNavigation
+from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
 
 
 class MainScreen(MDBottomNavigation):
     pass
 
+
+class Nominals(MDBottomNavigationItem):
+    pass
+
+
 class RadioHelperMD(MDApp):
     def build(self):
-        return MainScreen()
+        self.root = Builder.load_file("kv/main_screen.kv")
+        Builder.load_file("kv/nominals.kv")
 
 
 RadioHelperMD().run()

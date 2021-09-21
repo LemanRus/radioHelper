1e400489c7729b253d0325d3ee2f668550d83314
LemanRus <wlad163163@gmail.com>
2021-09-21 20:33:49 +0400

    v0.0.2 Реализована нижняя панель навигации

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from kivy.factory import Factory
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
 
 
-class MainScreen(MDBottomNavigation):
+class Markings(Screen):
     pass
 
 
-class Nominals(MDBottomNavigationItem):
+class Calculations(Screen):
+    pass
+
+
+class AboutScreen(Screen):
     pass
 
 
 class RadioHelperMD(MDApp):
     def build(self):
-        self.root = Builder.load_file("kv/main_screen.kv")
-        Builder.load_file("kv/nominals.kv")
+        Builder.load_file("kv/markings.kv")
+        Builder.load_file("kv/calculations.kv")
+        Builder.load_file("kv/about_screen.kv")
+        return Builder.load_file("kv/main.kv")
 
 
 RadioHelperMD().run()

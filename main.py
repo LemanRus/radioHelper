9d7871d22699776f350990428be9f5d51644b78c
LemanRus <wlad163163@gmail.com>
2021-09-23 15:06:41 +0400

    v0.0.2 Экран резисторов в отдельном .kv файле

--- main.py
+++ main.py
@@ -19,12 +19,17 @@ class AboutScreen(MDScreen):
     pass
 
 
+class ResistorsMarking(MDScreen):
+    pass
+
+
 class RadioHelperMD(MDApp):
     def build(self):
         Builder.load_file("kv/markings.kv")
         Builder.load_file("kv/calculations.kv")
         Builder.load_file("kv/about_screen.kv")
         Builder.load_file("kv/nominals.kv")
+        Builder.load_file("kv/resistors_marking.kv")
         return Builder.load_file("kv/main.kv")
 
 

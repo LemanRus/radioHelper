43d6ab2ce8242c9a6bcbd0c81820318fe28fe3b9
LemanRus <wlad163163@gmail.com>
2022-03-09 22:55:41 +0400

    v0.0.8 Изменен расчёт делителя напряжения - редизайн

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.card import MDCard
 from kivymd.uix.list import IRightBodyTouch, OneLineListItem
+from StandardRows import StandardRows
 
 import ResistorsMarking, CapacitorsMarking, ResistorLEDCalculateScreen, InductorCalculateTurns, \
-    ParallelResistorCalculateScreen, SerialCapacitorCalculateScreen, VoltageDividerCalculateScreen, LM317Voltage, \
+    ParallelResistorCalculateScreen, SerialCapacitorCalculateScreen, LM317Voltage, \
     LM317Current, InductorCalculateHenrys
 
 from kivy.lang import Builder
@@ -48,12 +49,68 @@ class LM317CalculateScreen(MDScreen):
     pass
 
 
-class VoltageDividerVout(MDScreen):
+class VoltageDividerCalculateScreen(MDScreen):
     pass
+
+
+class VoltageDividerVout(MDScreen):
+    def divider_calculate_vout(self, vin, r1, r2):
+        try:
+            vin = float(vin)
+            r1 = float(r1)
+            r2 = float(r2)
+
+            vout = r2 * vin / (r1 + r2)
+            rate = vin / vout
+
+            self.ids.v_out.text = "{:g}".format(vout)
+            self.ids.divider_rate.text = "{:g}".format(rate)
+        except Exception:
+            self.ids.v_out.text = "Неверный ввод!"
+            self.ids.divider_rate.text = ""
 
 
 class VoltageDividerR2(MDScreen):
-    pass
+    def divider_calculate_r(self, vin, vout, r1):
+        try:
+            vin = float(vin)
+            vout = float(vout)
+            r1 = float(r1)
+
+            if vin <= vout:
+                self.ids.r2_calculated.text = "Проверьте напряжения!"
+                self.ids.divider_rate_r.text = ""
+            else:
+                r2 = r1 * vout / (vin - vout)
+                rate = vin / vout
+
+                self.ids.r2_calculated.text = "{:g}".format(r2)
+                if r2 == 0:
+                    self.ids.r2_calculated.text = "0 Ом (перемычка)"
+                elif r2 < 1000:
+                    self.ids.r2_calculated.text = "{:g} Ом".format(r2)
+                elif r2 < 1000000:
+                    self.ids.r2_calculated.text = "{:g} кОм".format(r2 / 1000)
+                else:
+                    self.ids.r2_calculated.text = "{:g} МОм".format(r2 / 1000000)
+
+                self.ids.divider_rate_r.text = "{:g}".format(rate)
+
+                e6_result = StandardRows.calculate_standard_resistor(r2, False)
+                if e6_result == 0:
+                    self.ids.r2_e24.text = "0 Ом (перемычка)"
+                elif e6_result < 1000:
+                    self.ids.r2_e24.text = "{:g} Ом".format(e6_result)
+                elif e6_result < 1000000:
+                    self.ids.r2_e24.text = "{:g} кОм".format(e6_result / 1000)
+                else:
+                    self.ids.r2_e24.text = "{:g} МОм".format(e6_result / 1000000)
+
+                vout_corrected = e6_result * vin / (r1 + e6_result)
+                self.ids.vout_e24.text = "{:g} В".format(vout_corrected)
+        except (ZeroDivisionError, ValueError):
+            self.ids.r2_calculated.text = "Неверный ввод!"
+            self.ids.divider_rate_r.text = ""
 
 
 class InductorCalculateScreen(MDScreen):
